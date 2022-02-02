54a6e0cb1c95336c0c9d54a1c8adba98c67fd99a
lacj@kea.dk <lacj@kea.dk>
2022-02-02 19:37:12 +0100

    start

---
+++ script.js
@@ -0,0 +1,9 @@
+const allDivs = document.querySelectorAll("main>div");
+
+// allDivs.forEach(divEl => {
+//     divEl.getElementsByClassName.backgroundColor = `hsl(230, 70%, 70%)`;
+// });
+
+allDivs.forEach((divEl) => {
+  divEl.getElementsByClassName.backgroundColor = `hsl(230, 70%, 70%)`;
+});
